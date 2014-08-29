160cc295634bda7ec8b086764aa3d4afa4e14235
Jonathan Howden <jonathanhowden@gmail.com>
2014-08-29 08:14:08 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MultiplayerChess::Application.config.secret_key_base = '4ca2c3824544ecff3798fa44d07c9de8e73c860c62ff8842dfe4d76c3f0d5b5782efdcef943bf2c1f3684179bac139126a80d41af509bc9899a05d382dd99c1d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MultiplayerChess::Application.config.session_store :cookie_store, key: '_multiplayer_chess_session'

8aded4f16079ae16f7e74d8a60d7db5a00303805
Jonathan Howden <jhowden@jhowden.local>
2014-12-24 17:34:17 -0600

    Added winner migration to Game

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141129225726) do
+ActiveRecord::Schema.define(version: 20141224233005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20141129225726) do
     t.datetime "updated_at"
     t.integer  "player_turn"
     t.string   "user_input",    default: [], array: true
+    t.string   "winner"
   end
 
   create_table "invitations", force: true do |t|
